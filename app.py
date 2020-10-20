75d98968c042028c5b88237da93e1897f749c985
fox-dever <69247125+fox-dever@users.noreply.github.com>
2020-10-20 16:45:57 +0300

    add dreams parsing

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+from dreamparser import DreamParser
+
 def main():
-    pass
+    dream_parser = DreamParser(1)
+    
+    dream_html = dream_parser.get_next_dream_html()
+    dreams = []
+    
+    while dream_html:
+        dreams.append(dream_parser.get_dream(dream_html))
+        print(dreams[-1]["id"], dreams[-1]['title'])
+
+        dream_html = dream_parser.get_next_dream_html()
 
 
 if __name__ == '__main__':
